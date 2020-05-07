9c0c43c5d35bbfc6c9a6ca6c363a5ec3cf456146
J-A-C-E <J-A-C-E@users.noreply.github.com>
2020-05-07 08:48:33 -0500

    Proyecto

---
+++ PrinterLanguage/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace PrinterLanguage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.rtxtCodigo = new System.Windows.Forms.RichTextBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.lblNumRenglon = new System.Windows.Forms.Label();
+            this.txtNumRenglon = new System.Windows.Forms.TextBox();
+            this.btnCargarArchivo = new System.Windows.Forms.Button();
+            this.lblSubCadenaAEvaluar = new System.Windows.Forms.Label();
+            this.lblCadenaDeTokens = new System.Windows.Forms.Label();
+            this.txtSubCadenaAEvaluar = new System.Windows.Forms.TextBox();
+            this.txtCadenaDeTokens = new System.Windows.Forms.TextBox();
+            this.rtxtCodigoIntermedio = new System.Windows.Forms.RichTextBox();
+            this.lblCodigoIntermedio = new System.Windows.Forms.Label();
+            this.dgvIdentificadores = new System.Windows.Forms.DataGridView();
+            this.dgvConstantesNumericas = new System.Windows.Forms.DataGridView();
+            this.lblIdentificadores = new System.Windows.Forms.Label();
+            this.lblConstantesNumericas = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIdentificadores)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConstantesNumericas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtxtCodigo
+            // 
+            this.rtxtCodigo.BackColor = System.Drawing.Color.Black;
+            this.rtxtCodigo.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtCodigo.ForeColor = System.Drawing.Color.Crimson;
+            this.rtxtCodigo.Location = new System.Drawing.Point(12, 12);
+            this.rtxtCodigo.Name = "rtxtCodigo";
+            this.rtxtCodigo.Size = new System.Drawing.Size(400, 150);
+            this.rtxtCodigo.TabIndex = 0;
+            this.rtxtCodigo.Text = "";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.BackColor = System.Drawing.Color.Silver;
+            this.btnEjecutar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnEjecutar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEjecutar.Location = new System.Drawing.Point(445, 132);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(95, 30);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = false;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // lblNumRenglon
+            // 
+            this.lblNumRenglon.AutoSize = true;
+            this.lblNumRenglon.Location = new System.Drawing.Point(441, 59);
+            this.lblNumRenglon.Name = "lblNumRenglon";
+            this.lblNumRenglon.Size = new System.Drawing.Size(110, 19);
+            this.lblNumRenglon.TabIndex = 0;
+            this.lblNumRenglon.Text = "# de renglón";
+            // 
+            // txtNumRenglon
+            // 
+            this.txtNumRenglon.Enabled = false;
+            this.txtNumRenglon.Location = new System.Drawing.Point(469, 86);
+            this.txtNumRenglon.Name = "txtNumRenglon";
+            this.txtNumRenglon.Size = new System.Drawing.Size(50, 27);
+            this.txtNumRenglon.TabIndex = 0;
+            this.txtNumRenglon.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCargarArchivo
+            // 
+            this.btnCargarArchivo.BackColor = System.Drawing.Color.Silver;
+            this.btnCargarArchivo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnCargarArchivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargarArchivo.Location = new System.Drawing.Point(422, 12);
+            this.btnCargarArchivo.Name = "btnCargarArchivo";
+            this.btnCargarArchivo.Size = new System.Drawing.Size(150, 30);
+            this.btnCargarArchivo.TabIndex = 0;
+            this.btnCargarArchivo.Text = "Cargar Archivo";
+            this.btnCargarArchivo.UseVisualStyleBackColor = false;
+            this.btnCargarArchivo.Click += new System.EventHandler(this.btnCargarArchivo_Click);
+            // 
+            // lblSubCadenaAEvaluar
+            // 
+            this.lblSubCadenaAEvaluar.AutoSize = true;
+            this.lblSubCadenaAEvaluar.Location = new System.Drawing.Point(12, 175);
+            this.lblSubCadenaAEvaluar.Name = "lblSubCadenaAEvaluar";
+            this.lblSubCadenaAEvaluar.Size = new System.Drawing.Size(183, 19);
+            this.lblSubCadenaAEvaluar.TabIndex = 0;
+            this.lblSubCadenaAEvaluar.Text = "Sub-cadena a evaluar";
+            // 
+            // lblCadenaDeTokens
+            // 
+            this.lblCadenaDeTokens.AutoSize = true;
+            this.lblCadenaDeTokens.Location = new System.Drawing.Point(12, 237);
+            this.lblCadenaDeTokens.Name = "lblCadenaDeTokens";
+            this.lblCadenaDeTokens.Size = new System.Drawing.Size(156, 19);
+            this.lblCadenaDeTokens.TabIndex = 0;
+            this.lblCadenaDeTokens.Text = "Cadena de tokens";
+            // 
+            // txtSubCadenaAEvaluar
+            // 
+            this.txtSubCadenaAEvaluar.Location = new System.Drawing.Point(12, 197);
+            this.txtSubCadenaAEvaluar.Name = "txtSubCadenaAEvaluar";
+            this.txtSubCadenaAEvaluar.Size = new System.Drawing.Size(400, 27);
+            this.txtSubCadenaAEvaluar.TabIndex = 0;
+            // 
+            // txtCadenaDeTokens
+            // 
+            this.txtCadenaDeTokens.Location = new System.Drawing.Point(12, 259);
+            this.txtCadenaDeTokens.Name = "txtCadenaDeTokens";
+            this.txtCadenaDeTokens.Size = new System.Drawing.Size(400, 27);
+            this.txtCadenaDeTokens.TabIndex = 0;
+            // 
+            // rtxtCodigoIntermedio
+            // 
+            this.rtxtCodigoIntermedio.BackColor = System.Drawing.Color.Black;
+            this.rtxtCodigoIntermedio.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtCodigoIntermedio.ForeColor = System.Drawing.Color.White;
+            this.rtxtCodigoIntermedio.Location = new System.Drawing.Point(12, 321);
+            this.rtxtCodigoIntermedio.Name = "rtxtCodigoIntermedio";
+            this.rtxtCodigoIntermedio.Size = new System.Drawing.Size(400, 150);
+            this.rtxtCodigoIntermedio.TabIndex = 0;
+            this.rtxtCodigoIntermedio.Text = "";
+            // 
+            // lblCodigoIntermedio
+            // 
+            this.lblCodigoIntermedio.AutoSize = true;
+            this.lblCodigoIntermedio.Location = new System.Drawing.Point(12, 299);
+            this.lblCodigoIntermedio.Name = "lblCodigoIntermedio";
+            this.lblCodigoIntermedio.Size = new System.Drawing.Size(160, 19);
+            this.lblCodigoIntermedio.TabIndex = 0;
+            this.lblCodigoIntermedio.Text = "Código Intermedio";
+            // 
+            // dgvIdentificadores
+            // 
+            this.dgvIdentificadores.AllowUserToAddRows = false;
+            this.dgvIdentificadores.AllowUserToDeleteRows = false;
+            this.dgvIdentificadores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvIdentificadores.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle17;
+            this.dgvIdentificadores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle18.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvIdentificadores.DefaultCellStyle = dataGridViewCellStyle18;
+            this.dgvIdentificadores.Location = new System.Drawing.Point(12, 500);
+            this.dgvIdentificadores.Name = "dgvIdentificadores";
+            this.dgvIdentificadores.Size = new System.Drawing.Size(275, 150);
+            this.dgvIdentificadores.TabIndex = 0;
+            // 
+            // dgvConstantesNumericas
+            // 
+            this.dgvConstantesNumericas.AllowUserToAddRows = false;
+            this.dgvConstantesNumericas.AllowUserToDeleteRows = false;
+            this.dgvConstantesNumericas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle19.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle19.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvConstantesNumericas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle19;
+            this.dgvConstantesNumericas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle20.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle20.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle20.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle20.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle20.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle20.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvConstantesNumericas.DefaultCellStyle = dataGridViewCellStyle20;
+            this.dgvConstantesNumericas.Location = new System.Drawing.Point(297, 500);
+            this.dgvConstantesNumericas.Name = "dgvConstantesNumericas";
+            this.dgvConstantesNumericas.Size = new System.Drawing.Size(275, 150);
+            this.dgvConstantesNumericas.TabIndex = 0;
+            // 
+            // lblIdentificadores
+            // 
+            this.lblIdentificadores.AutoSize = true;
+            this.lblIdentificadores.Location = new System.Drawing.Point(87, 478);
+            this.lblIdentificadores.Name = "lblIdentificadores";
+            this.lblIdentificadores.Size = new System.Drawing.Size(129, 19);
+            this.lblIdentificadores.TabIndex = 0;
+            this.lblIdentificadores.Text = "Identificadores";
+            // 
+            // lblConstantesNumericas
+            // 
+            this.lblConstantesNumericas.AutoSize = true;
+            this.lblConstantesNumericas.Location = new System.Drawing.Point(341, 478);
+            this.lblConstantesNumericas.Name = "lblConstantesNumericas";
+            this.lblConstantesNumericas.Size = new System.Drawing.Size(188, 19);
+            this.lblConstantesNumericas.TabIndex = 0;
+            this.lblConstantesNumericas.Text = "Constantes Numéricas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(584, 662);
+            this.Controls.Add(this.lblConstantesNumericas);
+            this.Controls.Add(this.lblIdentificadores);
+            this.Controls.Add(this.dgvConstantesNumericas);
+            this.Controls.Add(this.dgvIdentificadores);
+            this.Controls.Add(this.lblCodigoIntermedio);
+            this.Controls.Add(this.rtxtCodigoIntermedio);
+            this.Controls.Add(this.txtCadenaDeTokens);
+            this.Controls.Add(this.txtSubCadenaAEvaluar);
+            this.Controls.Add(this.lblCadenaDeTokens);
+            this.Controls.Add(this.lblSubCadenaAEvaluar);
+            this.Controls.Add(this.btnCargarArchivo);
+            this.Controls.Add(this.txtNumRenglon);
+            this.Controls.Add(this.lblNumRenglon);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.rtxtCodigo);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Printer Language";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIdentificadores)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConstantesNumericas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtCodigo;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Label lblNumRenglon;
+        private System.Windows.Forms.TextBox txtNumRenglon;
+        private System.Windows.Forms.Button btnCargarArchivo;
+        private System.Windows.Forms.Label lblSubCadenaAEvaluar;
+        private System.Windows.Forms.Label lblCadenaDeTokens;
+        private System.Windows.Forms.TextBox txtSubCadenaAEvaluar;
+        private System.Windows.Forms.TextBox txtCadenaDeTokens;
+        private System.Windows.Forms.RichTextBox rtxtCodigoIntermedio;
+        private System.Windows.Forms.Label lblCodigoIntermedio;
+        private System.Windows.Forms.DataGridView dgvIdentificadores;
+        private System.Windows.Forms.DataGridView dgvConstantesNumericas;
+        private System.Windows.Forms.Label lblIdentificadores;
+        private System.Windows.Forms.Label lblConstantesNumericas;
+    }
+}
+
---
+++ PrinterLanguage/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace PrinterLanguage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string s = "";
+        static string p = "";
+        static string u = "";
+        static string po = "";
+        static string cadenaConexion = "server=" + s + ";port=" + p + ";Database=printerlanguage" + ";UID=" + u +";password=" + po + ";";
+
+        private void btnCargarArchivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Text Documents|*.txt";
+            open.Title = "Open";
+            open.FileName = "";
+            var o = open.ShowDialog();
+
+            if (o == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(open.FileName);
+                rtxtCodigo.Text = read.ReadToEnd();
+                read.Close();
+            }
+
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            borrarDatosEnTablas();
+            rtxtCodigoIntermedio.Clear();
+            string cadena = "";
+            string subcadena = "";
+            string apuntador = "0";
+            string token = "";
+            bool esp = false;
+            for (int x = 0; x < rtxtCodigo.Lines.Count(); x++)
+            {
+                txtNumRenglon.Text = (x + 1).ToString();
+                cadena = rtxtCodigo.Lines[x];
+                txtSubCadenaAEvaluar.Text = cadena;
+
+                for (int y = 0; y < cadena.Length; y++)
+                {
+                    if(cadena[y].ToString() == "[" || cadena[y].ToString() == "\\")
+                    {
+                        if(cadena[y].ToString() == "\\" && cadena[y+1].ToString() == " ")
+                        {
+                            esp = false;
+                        }
+                        else
+                        {
+                            esp = true;
+                        }
+                    }
+                    else if(cadena[y].ToString() == "]")
+                    {
+                        esp = false;
+                    }
+
+                    if (cadena[y].ToString() == " ")
+                    {
+                        if (esp)
+                        {
+                            MessageBox.Show("ESPACIO \n Apuntador: " + apuntador, "Datos del recorrido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            apuntador = Recorrer(apuntador, "DEL");
+                            token = Recorrer(apuntador, "TOKEN");
+
+                            if (token == "IDE")
+                            {
+                                token = agregarDatosEnTablas("IDENTIFICADOR", token, subcadena);
+                                mostrarDatosEnTablas();
+                            }
+                            else if (token == "CNU")
+                            {
+                                token = agregarDatosEnTablas("CONSTANTENUMERICA", token, subcadena);
+                                mostrarDatosEnTablas();
+                            }
+
+                            txtCadenaDeTokens.Text = txtCadenaDeTokens.Text + token + " ";
+                            apuntador = "0";
+                            subcadena = "";
+                        }
+                    }
+                    else
+                    {
+                        apuntador = Recorrer(apuntador, "`A" + cadena[y].ToString() + "`");
+                        subcadena = subcadena + cadena[y].ToString();
+                    }
+                }
+                MessageBox.Show("¡Subcadena evaluada con éxito!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                rtxtCodigoIntermedio.Text += txtCadenaDeTokens.Text + "\n";
+                txtSubCadenaAEvaluar.Clear();
+                txtCadenaDeTokens.Clear();
+            }
+            txtNumRenglon.Clear();
+            MessageBox.Show("¡Cadena totalmente evaluada con éxito!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        public string Recorrer(string apuntador, string caracter)
+        {
+            MySqlConnection mySQLCon = new MySqlConnection(cadenaConexion);
+
+            mySQLCon.Open();
+            MySqlDataReader myDtRd;
+            MySqlCommand myQuery = new MySqlCommand("SELECT " +caracter + " FROM M WHERE ID = " + apuntador, mySQLCon);
+            myDtRd = myQuery.ExecuteReader();
+            string token = "";
+            string nomCol = "";
+            string a = "";
+            while (myDtRd.Read())
+            {
+                if(myDtRd.GetName(0) == "TOKEN")
+                {
+                    nomCol = myDtRd.GetName(0);
+                    token = myDtRd.GetString(0);
+                    mySQLCon.Close();
+                    MessageBox.Show("Columna: " + nomCol + "\n Token: " + token, "Datos del recorrido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return token;
+                }
+                else
+                {
+                    nomCol = myDtRd.GetName(0);
+                    a = myDtRd.GetString(0);
+                    MessageBox.Show("Columna: " + nomCol + "\n Apuntador: " + a, "Datos del recorrido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            mySQLCon.Close();
+            return a;
+        }
+
+        public string agregarDatosEnTablas(string tabla, string token, string contenido)
+        {
+            MySqlConnection mySQLCon = new MySqlConnection(cadenaConexion);
+
+            mySQLCon.Open();
+            MySqlDataReader myDtRd1;
+            MySqlCommand myQuery1 = new MySqlCommand("SELECT COUNT(*) FROM " + tabla, mySQLCon);
+            myDtRd1 = myQuery1.ExecuteReader();
+            int filas = 0;
+            while (myDtRd1.Read())
+            {
+                filas = myDtRd1.GetInt32(0);
+            }
+            mySQLCon.Close();
+
+            if (filas == 0)
+            {
+                mySQLCon.Open();
+                MySqlCommand myQuery2 = new MySqlCommand("INSERT INTO " + tabla + " (TOKEN, CONTENIDO) VALUES ( '" + token + "1', '" + contenido + "')", mySQLCon);
+                myQuery2.ExecuteNonQuery();
+                mySQLCon.Close();
+                return token + 1;
+            }
+            else
+            {
+                mySQLCon.Open();
+                MySqlDataReader myDtRd2;
+                MySqlCommand myQuery3 = new MySqlCommand("SELECT * FROM " + tabla + " WHERE CONTENIDO = '" + contenido + "'", mySQLCon);
+                myDtRd2 = myQuery3.ExecuteReader();
+                string t = "";
+                string c = "";
+                while (myDtRd2.Read())
+                {
+                    t = myDtRd2.GetString(0);
+                    c = myDtRd2.GetString(1);
+                }
+                mySQLCon.Close();
+
+                if(c == contenido)
+                {
+                    return t;
+                }
+                else
+                {
+                    mySQLCon.Open();
+                    MySqlCommand myQuery4 = new MySqlCommand("INSERT INTO " + tabla + " (TOKEN, CONTENIDO) VALUES ( '" + (token + (filas + 1)) + "', '" + contenido + "')", mySQLCon);
+                    myQuery4.ExecuteNonQuery();
+                    mySQLCon.Close();
+                    return token + (filas + 1);
+                }
+            }
+        }
+
+        public void mostrarDatosEnTablas()
+        {
+            MySqlConnection mySQLCon = new MySqlConnection(cadenaConexion);
+
+            mySQLCon.Open();
+            MySqlDataAdapter myDtAd1 = new MySqlDataAdapter("SELECT * FROM IDENTIFICADOR", mySQLCon);
+            DataTable myDtTb1 = new DataTable();
+            myDtAd1.Fill(myDtTb1);
+            dgvIdentificadores.DataSource = myDtTb1;
+            mySQLCon.Close();
+
+            mySQLCon.Open();
+            MySqlDataAdapter myDtAd2 = new MySqlDataAdapter("SELECT * FROM CONSTANTENUMERICA", mySQLCon);
+            DataTable myDtTb2 = new DataTable();
+            myDtAd2.Fill(myDtTb2);
+            dgvConstantesNumericas.DataSource = myDtTb2;
+            mySQLCon.Close();
+
+        }
+
+        public void borrarDatosEnTablas()
+        {
+            MySqlConnection mySQLCon = new MySqlConnection(cadenaConexion);
+
+            mySQLCon.Open();
+            MySqlDataReader myDtRd1;
+            MySqlCommand myQuery1 = new MySqlCommand("SELECT COUNT(*) FROM IDENTIFICADOR", mySQLCon);
+            myDtRd1 = myQuery1.ExecuteReader();
+            int id = 0;
+            while (myDtRd1.Read())
+            {
+                id = myDtRd1.GetInt32(0);
+            }
+            mySQLCon.Close();
+
+            mySQLCon.Open();
+            MySqlDataReader myDtRd2;
+            MySqlCommand myQuery2 = new MySqlCommand("SELECT COUNT(*) FROM CONSTANTENUMERICA", mySQLCon);
+            myDtRd2 = myQuery2.ExecuteReader();
+            int cn = 0;
+            while (myDtRd2.Read())
+            {
+                cn = myDtRd2.GetInt32(0);
+            }
+            mySQLCon.Close();
+
+            if(id > 0)
+            {
+                mySQLCon.Open();
+                MySqlCommand myQuery3 = new MySqlCommand("TRUNCATE IDENTIFICADOR", mySQLCon);
+                myQuery3.ExecuteNonQuery();
+                mySQLCon.Close();
+            }
+            
+            if(cn > 0)
+            {
+                mySQLCon.Open();
+                MySqlCommand myQuery4 = new MySqlCommand("TRUNCATE CONSTANTENUMERICA", mySQLCon);
+                myQuery4.ExecuteNonQuery();
+                mySQLCon.Close();
+            }
+            mostrarDatosEnTablas();
+        }
+    }
+}
